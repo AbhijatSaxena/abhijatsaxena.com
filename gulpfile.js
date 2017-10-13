8a00c7cd545b7501642256b318280a75621e1012
Abhijat Saxena <abhijatsaxena@Abhijats-MacBook-Pro.local>
2017-10-14 02:33:48 +0530

    testing output using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
+
 gulp.task('deploy', function() {
+
+    process.stdout.write("Hello");
+
     var remotePath = '/public_html/';
     var conn = ftp.create({
         host: 'ftp.byethost14.com',
