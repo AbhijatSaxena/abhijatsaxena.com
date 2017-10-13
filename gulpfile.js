9f702d54a2143eab3bcd9043accfe36b7b614d30
Abhijat Saxena <abhijatsaxena@Abhijats-MacBook-Pro.local>
2017-10-14 04:00:02 +0530

    trying for parallel ftp transfers to reduce the latency

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ gulp.task('deploy', function() {
         host: process.env.FTP_HOST,
         user: process.env.FTP_USER,
         password: process.env.FTP_PASS,
+        parallel: 10,
         log: gutil.log
     });
 
