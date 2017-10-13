44d5152078e73720b4de69730e20c702fa435e09
Abhijat Saxena <abhijatsaxena@Abhijats-MacBook-Pro.local>
2017-10-14 03:12:40 +0530

    changed name to Abhijat Saxena

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp');
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
-var minimist = require('minimist');
-// var args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
     var remotePath = '/htdocs/';
